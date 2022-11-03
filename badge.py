b834e1825125d94db1531f133b9ce2e777cac6e5
driller <eleshis@gmail.com>
2022-11-03 16:12:49 +0900

    add Series

---
+++ badge.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import nbformat
+from git import Repo
+
+
+def get_repository_name(dir_path="."):
+    repo = Repo(dir_path)
+    origin_url = repo.remotes.origin.url
+    return origin_url.split(":")[1].replace(".git", "")
+
+
+def gen_badge_text(repository_name, file_path):
+    return f"[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](http://colab.research.google.com/github/{repository_name}/blob/main/{file_path})"
+
+
+def add_badge(repository_name, file_path):
+    nb = nbformat.read(file_path, as_version=4)
+    first = nb["cells"][0]
+    first_cell_type = first["cell_type"]
+    first_source = first["source"]
+    if first_cell_type == "markdown" and first_source.startswith("[![Open In Colab]"):
+        return
+    source = gen_badge_text(repository_name, file_path)
+    new_cell = nbformat.v4.new_markdown_cell(source=source)
+    nb["cells"].insert(0, new_cell)
+    new_nb = nbformat.v4.new_notebook()
+    new_nb["cells"] = nb["cells"]
+    nbformat.write(new_nb, file_path, version=4)
+
+
+base_dir = "docs"
+repository_name = get_repository_name()
+
+p = Path(base_dir)
+for file_path in p.glob("**/*.ipynb"):
+    add_badge(repository_name, file_path)
